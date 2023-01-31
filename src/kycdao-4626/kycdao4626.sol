5857c9e9e1ce5468a08f05a1a0414ff0bfd8e085
vikramarun <vikram@stargate.money>
2023-01-30 21:39:29 -0500

    chore: add implementation

---
+++ src/kycdao-4626/kycdao4626.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.14;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC4626} from "solmate/mixins/ERC4626.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+import {KYCDAO} from "./interfaces/IKycValidity.sol";
+
+/// @notice NFT-gated ERC-4626 using KYCDAO
+/// @author ZeroPoint Labs
+contract kycDAO4626 is ERC4626 {
+
+    IKycValidity public kycValidity;
+
+    modifier hasKYC() {
+        require(kycValidity.hasValidToken(msg.sender), "You must have a valid KYC token to use this contract");
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Libraries usage
+    /// -----------------------------------------------------------------------
+
+    using FixedPointMathLib for uint256;
+    using SafeTransferLib for ERC20;
+
+    /// -----------------------------------------------------------------------
+    /// Constructor
+    /// -----------------------------------------------------------------------
+
+    constructor(ERC20 asset_, address _kycValidity) 
+        ERC4626(asset_,_vaultName(asset_),_vaultSymbol(asset_)) {
+        kycValidity = IKycValidity(_kycValidity);
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC4626 overrides
+    /// -----------------------------------------------------------------------
+
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256 shares) hasKYC() {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function mint(uint256 shares, address receiver) public virtual returns (uint256 assets) hasKYC() {
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) hasKYC() {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 assets) hasKYC() {
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC20 metadata generation
+    /// -----------------------------------------------------------------------
+
+    function _vaultName(ERC20 asset_) internal view virtual returns (string memory vaultName) {
+        vaultName = string.concat("kycERC4626-", asset_.symbol());
+    }
+
+    function _vaultSymbol(ERC20 asset_) internal view virtual returns (string memory vaultSymbol) {
+        vaultSymbol = string.concat("kyc", asset_.symbol());
+    }
+}
