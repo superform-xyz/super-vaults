ead8eee3d989af1269cb66c3b66c5858e3b264b7
sidduHERE <elugamsudhiswar@gmail.com>
2023-02-13 19:10:37 -0500

    fix: AaveV2ERC4626Reinvest and its factory

--- src/aave-v2/AaveV2ERC4626Reinvest.sol
+++ src/aave-v2/AaveV2ERC4626Reinvest.sol
@@ -14,33 +14,35 @@ import {DexSwap} from "./utils/swapUtils.sol";
 /// @dev Reinvests rewards accrued for higher APY
 contract AaveV2ERC4626Reinvest is ERC4626 {
     
-    address public immutable manager;
-    address public rewardToken;
-    /// -----------------------------------------------------------------------
-    /// Libraries usage
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      LIBRARIES USED
+    //////////////////////////////////////////////////////////////*/
 
     using SafeTransferLib for ERC20;
 
-    /// -----------------------------------------------------------------------
-    /// Constants
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      CONSTANTS
+    //////////////////////////////////////////////////////////////*/
 
     uint256 internal constant ACTIVE_MASK =
         0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFF;
     uint256 internal constant FROZEN_MASK =
         0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDFFFFFFFFFFFFFF;
 
-    /// -----------------------------------------------------------------------
-    /// Errors
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      ERRORS
+    //////////////////////////////////////////////////////////////*/
 
-        // @notice Thrown when reinvested amounts are not enough.
+    /// @notice Thrown when reinvested amounts are not enough.
     error MIN_AMOUNT_ERROR();
+    /// @notice Thrown when trying to call a function that is restricted
+    error INVALID_ACCESS();
+    /// @notice Thrown when trying to redeem shares worth 0 assets
+    error ZERO_ASSETS();
 
-    /// -----------------------------------------------------------------------
-    /// Immutable params
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      IMMUTABLES & VARIABLES
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice The Aave aToken contract (rebasing)
     ERC20 public immutable aToken;
@@ -65,10 +67,22 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         address pair2;
     }
 
-    /// -----------------------------------------------------------------------
-    /// Constructor
-    /// -----------------------------------------------------------------------
+    /// @notice Manager for setting swap routes for harvest() per each vault
+    address public immutable manager;
+    /// @notice address of reward token from AAVE liquidity mining
+    address public rewardToken;
 
+    /*//////////////////////////////////////////////////////////////
+                      CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Create a new AaveV2ERC4626Reinvest
+    /// @param asset_ The underlying asset
+    /// @param aToken_ The Aave aToken contract
+    /// @param rewards_ The Aave-fork liquidity mining contract
+    /// @param lendingPool_ The Aave LendingPool contract
+    /// @param rewardToken_ The Aave-fork liquidity mining reward token
+    /// @param manager_ The manager for setting swap routes for harvest() per each vault
     constructor(
         ERC20 asset_,
         ERC20 aToken_,
@@ -87,23 +101,27 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         rewardsSet = false;
     }
 
-    /// -----------------------------------------------------------------------
-    /// AAVE-Fork Rewards Module
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      AAVE-FORK REWARDS
+    //////////////////////////////////////////////////////////////*/
     
     /// @notice Set swap routes for selling rewards
     /// @dev Setting wrong addresses here will revert harvest() calls
+    /// @param token_ address of intermediary token with high liquidity (no direct pools)
+    /// @param pair1_ address of pairToken (pool) for first swap (rewardToken => high liquidity token)
+    /// @param pair2_ address of pairToken (pool) for second swap (high liquidity token => asset)
     function setRoute(
-        address token,
-        address pair1,
-        address pair2
+        address token_,
+        address pair1_,
+        address pair2_
     ) external {
-        require(msg.sender == manager, "onlyOwner");
-        SwapInfo = swapInfo(token, pair1, pair2);
+        if(msg.sender != manager) revert INVALID_ACCESS();
+        SwapInfo = swapInfo(token_, pair1_, pair2_);
         rewardsSet = true;
     }
 
     /// @notice Claims liquidity providing rewards from AAVE-Fork and performs low-lvl swap with instant reinvesting
+    /// @param minAmountOut_ minimum amount of asset to receive after 2 swaps
     function harvest(uint256 minAmountOut_) external {
 
         /// @dev Claim rewards from AAVE-Fork
@@ -159,79 +177,93 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         return rewards.getUserUnclaimedRewards(address(this));
     }
 
-    /// -----------------------------------------------------------------------
-    /// ERC4626 overrides
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      ERC4626 OVERRIDES
+    //////////////////////////////////////////////////////////////*/
 
+    ///@notice Withdraws assets from Aave and burns shares
+    ///@param assets_ Amount of assets to withdraw
+    ///@param receiver_ Address to send withdrawn assets to
+    ///@param owner_ Address to burn shares from
     function withdraw(
-        uint256 assets,
-        address receiver,
-        address owner
+        uint256 assets_,
+        address receiver_,
+        address owner_
     ) public virtual override returns (uint256 shares) {
-        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+        shares = previewWithdraw(assets_); // No need to check for rounding error, previewWithdraw rounds up.
 
-        if (msg.sender != owner) {
-            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+        if (msg.sender != owner_) {
+            uint256 allowed = allowance[owner_][msg.sender]; // Saves gas for limited approvals.
 
             if (allowed != type(uint256).max)
-                allowance[owner][msg.sender] = allowed - shares;
+                allowance[owner_][msg.sender] = allowed - shares;
         }
 
-        beforeWithdraw(assets, shares);
+        beforeWithdraw(assets_, shares);
 
-        _burn(owner, shares);
+        _burn(owner_, shares);
 
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+        emit Withdraw(msg.sender, receiver_, owner_, assets_, shares);
 
         // withdraw assets directly from Aave
-        lendingPool.withdraw(address(asset), assets, receiver);
+        lendingPool.withdraw(address(asset), assets_, receiver_);
     }
 
+    ///@notice Redeems assets from Aave and burns shares
+    ///@param shares_ Amount of shares to redeem
+    ///@param receiver_ Address to send redeemed assets to
+    ///@param owner_ Address to burn shares from
     function redeem(
-        uint256 shares,
-        address receiver,
-        address owner
+        uint256 shares_,
+        address receiver_,
+        address owner_
     ) public virtual override returns (uint256 assets) {
-        if (msg.sender != owner) {
-            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+        if (msg.sender != owner_) {
+            uint256 allowed = allowance[owner_][msg.sender]; // Saves gas for limited approvals.
 
             if (allowed != type(uint256).max)
-                allowance[owner][msg.sender] = allowed - shares;
+                allowance[owner_][msg.sender] = allowed - shares_;
         }
 
         // Check for rounding error since we round down in previewRedeem.
-        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+        if((assets = previewRedeem(shares_)) == 0) {
+            revert ZERO_ASSETS();
+        }
 
-        beforeWithdraw(assets, shares);
+        beforeWithdraw(assets, shares_);
 
-        _burn(owner, shares);
+        _burn(owner_, shares_);
 
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+        emit Withdraw(msg.sender, receiver_, owner_, assets, shares_);
 
         // withdraw assets directly from Aave
-        lendingPool.withdraw(address(asset), assets, receiver);
+        lendingPool.withdraw(address(asset), assets, receiver_);
     }
 
+    ///@notice returns total a tokens in the vault
     function totalAssets() public view virtual override returns (uint256) {
         // aTokens use rebasing to accrue interest, so the total assets is just the aToken balance
         return aToken.balanceOf(address(this));
     }
 
+    ///@notice called by deposit and mint to deposit assets into Aave
+    ///@param assets_ Amount of assets to deposit
     function afterDeposit(
-        uint256 assets,
-        uint256 /*shares*/
+        uint256 assets_,
+        uint256 /*shares_*/
     ) internal virtual override {
         /// -----------------------------------------------------------------------
         /// Deposit assets into Aave
         /// -----------------------------------------------------------------------
 
         // approve to lendingPool
-        asset.safeApprove(address(lendingPool), assets);
+        asset.safeApprove(address(lendingPool), assets_);
 
         // deposit into lendingPool
-        lendingPool.deposit(address(asset), assets, address(this), 0);
+        lendingPool.deposit(address(asset), assets_, address(this), 0);
     }
 
+    ///@notice returns max amount of assets that can be deposited
     function maxDeposit(address)
         public
         view
@@ -256,6 +288,7 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         return type(uint256).max;
     }
 
+    ///@notice returns max amount of assets that can be minted
     function maxMint(address) public view virtual override returns (uint256) {
         // check if pool is paused
         if (lendingPool.paused()) {
@@ -274,7 +307,9 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         return type(uint256).max;
     }
 
-    function maxWithdraw(address owner)
+    ///@notice returns max amount of assets that can be withdrawn
+    ///@param owner_ Address to check max withdraw for
+    function maxWithdraw(address owner_)
         public
         view
         virtual
@@ -296,11 +331,13 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         }
 
         uint256 cash = asset.balanceOf(address(aToken));
-        uint256 assetsBalance = convertToAssets(balanceOf[owner]);
+        uint256 assetsBalance = convertToAssets(balanceOf[owner_]);
         return cash < assetsBalance ? cash : assetsBalance;
     }
 
-    function maxRedeem(address owner)
+    ///@notice returns max amount of shares that can be redeemed
+    ///@param owner_ Address to check max redeem for
+    function maxRedeem(address owner_)
         public
         view
         virtual
@@ -323,13 +360,13 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
 
         uint256 cash = asset.balanceOf(address(aToken));
         uint256 cashInShares = convertToShares(cash);
-        uint256 shareBalance = balanceOf[owner];
+        uint256 shareBalance = balanceOf[owner_];
         return cashInShares < shareBalance ? cashInShares : shareBalance;
     }
 
-    /// -----------------------------------------------------------------------
-    /// ERC20 metadata generation
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      ERC20 METADATA 
+    //////////////////////////////////////////////////////////////*/
 
     function _vaultName(ERC20 asset_)
         internal
@@ -349,9 +386,9 @@ contract AaveV2ERC4626Reinvest is ERC4626 {
         vaultSymbol = string.concat("wa2-", asset_.symbol());
     }
 
-    /// -----------------------------------------------------------------------
-    /// Internal functions
-    /// -----------------------------------------------------------------------
+    /*//////////////////////////////////////////////////////////////
+                      INTERNAL HELPERS
+    //////////////////////////////////////////////////////////////*/
 
     function _getActive(uint256 configData) internal pure returns (bool) {
         return configData & ~ACTIVE_MASK != 0;
